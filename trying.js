11f45c72c3838b9a55492109afefb1797d13aeb8
Alejo <Alejo@Alejos-MacBook-Pro.local>
2018-03-06 08:24:08 +0100

    percent solution draft in trying.js

---
+++ trying.js
@@ -0,0 +1,23 @@
+let testArr = ["7", "8", "-", "1"];
+let number = [];
+let testArrLength = testArr.length
+for(let i=testArrLength - 1; i>=0; i--){
+  console.log(testArr[i]);
+    if(testArr[i] === '-'){
+        break;
+    }
+    number.unshift(testArr.pop());
+    
+    console.log(testArr);
+    
+}
+
+number.push('/', '100');
+numberJoined = number.join('');
+numberInpercent = eval(numberJoined);
+console.log(number);
+console.log(numberJoined);
+numberInpercent
+testArr.push(numberInpercent);
+testArr
+
